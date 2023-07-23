30bc6006e0b342c894240a7ec2c086bdef744105
leliasgomer <leliasgomer@gmail.com>
2023-07-23 00:06:45 -0500

    fix: bug: master change center from grid to flex

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -9,7 +9,7 @@ interface MediaCardProps {
 
 export default function MediaCard({link, color, padding = '', children}: MediaCardProps) {
   return (
-    <a aria-label="enlace a una red social" href={link} target="_blank" className={`${color} grid place-content-center cursor-pointer rounded-md aspect-square relative ${padding}`}>
+    <a aria-label="enlace a una red social" href={link} target="_blank" className={`${color} flex justify-center items-center cursor-pointer rounded-md aspect-square relative ${padding}`}>
         <Target />
         {children}
     </a>
