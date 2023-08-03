807f24924c225b91cb9fe66f66d8527ae7002dfd
leliasgomer <leliasgomer@gmail.com>
2023-08-03 14:19:35 -0500

    wip: :construction: master create next section

--- src/pages/NextJs.tsx
+++ src/pages/NextJs.tsx
@@ -1,9 +1,13 @@
+import ProjectCard from '../components/ProjectCard'
+import { nextjs } from '../data/nextjs'
+
 export default function NextJs () {
   return (
-    <section className="w-full border-2 border-yellow-400 h-full">
-      <div className="flex flex-wrap gap-5">
-        <article className="border md:w-[300px] w-full h-[300px]"></article>
-        <article className="border md:w-[300px] w-full h-[300px]"></article>
+    <section className="w-full h-full">
+      <div className="w-full h-full grid lg:grid-cols-3 grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5">
+        {nextjs.map(item => (
+          <ProjectCard key={item.title} item={item} />
+        ))}
       </div>
     </section>
   )
