6cdf2524b837278c0d25e0c8f15d47c977048916
leliasgomer <leliasgomer@gmail.com>
2023-08-04 13:07:55 -0500

    wip: :construction: master add an animate

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,7 +5,7 @@ export default function Projects () {
   return (
     <main className="w-full bg-bluebg min-h-screen flex justify-center sm:py-10 px-5 scroll-smooth text-white">
       <div className='effect pointer-events-none fixed top-0 left-0 w-full h-screen'></div>
-      <section className="gap-4 my-5 md:max-w-[1200px] w-full mx-auto flex flex-col">
+      <section className="gap-4 my-5 md:max-w-[1200px] w-full mx-auto flex flex-col animate-fade-down animate-duration-1000">
         <HeaderProjects />
         <Tabss />
       </section>
