95de31644349428720224ea5d2f8d04b4a4228a0
leliasgomer <leliasgomer@gmail.com>
2023-08-05 20:27:55 -0500

    wip: :construction: master improve data from nextjs

--- src/data/nextjs.ts
+++ src/data/nextjs.ts
@@ -6,7 +6,7 @@ import { NextJs, Tailwind, TypeScript, Zustand } from '../icons/skills'
 export const nextjs = [{
   title: 'Movie App',
   img: movie,
-  description: 'Inkfinity es un sitio web donde puedes explorar y añadir libros a tu lista de lectura. Utilizando React/Next.js y Tailwind, desarrollé esta prueba técnica para el evento de midudev. Con filtros por categoría, número de páginas y una función de búsqueda, encontrar tu libro favorito es fácil y rápido. La interfaz es intuitiva y responsive, brindando una experiencia de usuario fluida en cualquier dispositivo. Demuestro mis habilidades en desarrollo frontend al utilizar componentes en React y estilos con Tailwind CSS.',
+  description: 'This is a popular movie app, I made this app using NextJs technologies for the frontend taking advantage of the good optimization it gives to images, TailwindCSS for styling, Zustand to handle global state and TypeScript for better code and error avoidance clean. I reinforced my knowledge in REST API requests, asynchronous and synchronous code. Using the public MovieTMDB api to display all data and images in the app.',
   skills: [NextJs, TypeScript, Zustand, Tailwind],
   bg: 'bg-[#2070bb]',
   url: 'app-movies-ecru.vercel.app',
@@ -14,7 +14,7 @@ export const nextjs = [{
 }, {
   title: 'Guest the Character',
   img: guest,
-  description: 'Inkfinity es un sitio web donde puedes explorar y añadir libros a tu lista de lectura. Utilizando React/Next.js y Tailwind, desarrollé esta prueba técnica para el evento de midudev. Con filtros por categoría, número de páginas y una función de búsqueda, encontrar tu libro favorito es fácil y rápido. La interfaz es intuitiva y responsive, brindando una experiencia de usuario fluida en cualquier dispositivo. Demuestro mis habilidades en desarrollo frontend al utilizar componentes en React y estilos con Tailwind CSS.',
+  description: 'One of my favorite apps made to practice and improve my programming logic using the free Rick and Morty api to play with the data thus creating this guessing game. For its creation I used NextJs technologies for the creation of components, Zustand for a more optimal handling of the global state, TailwindCSS for the styles and TypeScript for a safer code and to handle asynchronous and synchronous data types.',
   skills: [NextJs, TypeScript, Zustand, Tailwind],
   bg: 'bg-[#b94e21]',
   url: 'https://rick-morty-game-eight.vercel.app',
@@ -22,7 +22,7 @@ export const nextjs = [{
 }, {
   title: 'ChatGPT Clone',
   img: chatgpt,
-  description: 'Inkfinity es un sitio web donde puedes explorar y añadir libros a tu lista de lectura. Utilizando React/Next.js y Tailwind, desarrollé esta prueba técnica para el evento de midudev. Con filtros por categoría, número de páginas y una función de búsqueda, encontrar tu libro favorito es fácil y rápido. La interfaz es intuitiva y responsive, brindando una experiencia de usuario fluida en cualquier dispositivo. Demuestro mis habilidades en desarrollo frontend al utilizar componentes en React y estilos con Tailwind CSS.',
+  description: 'One of my last projects made to use the ChatGPT api, a clone of this page to use code in the frontend as well as in the backend using the NextJs framework, handle asynchronous code for requests by medi ed axios. The technologies I used were NextJs, TailwindCSS for styling, TypeScript for safer code and Zustand for global state.',
   skills: [NextJs, TypeScript, Zustand, Tailwind],
   bg: 'bg-[#449b84]',
   url: 'chatgpt-clone-cafesit8.vercel.app',
