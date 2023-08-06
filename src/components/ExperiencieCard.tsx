529b76d0f56102441f87b3d1da29d95117cdcb0d
leliasgomer <leliasgomer@gmail.com>
2023-08-05 21:16:16 -0500

    fix: :bug: master add properties to the img

--- src/components/ExperiencieCard.tsx
+++ src/components/ExperiencieCard.tsx
@@ -9,7 +9,7 @@ export default function ExperiencieCard () {
         <h3 className='text-white z-20 font-baloo400 md:mb-3 md:mt-1 font-bold text-2xl md:text-[40px] text-[20px]'>Work Experience</h3>
         <p className='text-white sm:text-xl text-[12px]'>Here you can see my work and personal experience</p>
       </article>
-      <img className='absolute top-0 left-0 opacity-30 pointer-events-none w-full h-full object-cover' src={coding} alt="gif of coding" />
+      <img className='absolute top-0 left-0 opacity-30 pointer-events-none w-full h-full object-cover' width={572} height={278} src={coding} alt="gif of coding" />
     </div>
   )
 }
--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -10,7 +10,7 @@ export default function ProjectsCard () {
       <Arrow />
       <h2 className="text-white font-baloo400 md:mb-3 md:mt-1 font-bold text-2xl md:text-[40px] text-[20px]">Projects</h2>
       <p className='text-white sm:text-xl text-[12px]'>In this section you'll be able to see some of my projects</p>
-      <img className='absolute pointer-events-none opacity-20 bg-transparent object-cover w-full h-full top-0 left-0' src={simpson} alt="git de los simpson" />
+      <img className='absolute pointer-events-none opacity-20 bg-transparent object-cover w-full h-full top-0 left-0' width={572} height={278} src={simpson} alt="git de los simpson" />
     </div>
   )
 }
