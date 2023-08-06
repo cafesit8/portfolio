595e51854c239aa7e43027260fdd8b0ef5ee60fe
leliasgomer <leliasgomer@gmail.com>
2023-08-05 20:12:40 -0500

    wip: :construction: master add image and description of chat gpt project

--- src/data/nextjs.ts
+++ src/data/nextjs.ts
@@ -1,5 +1,6 @@
 import movie from '../assets/next/movieapp.webp'
 import guest from '../assets/next/rickmortygame.webp'
+import chatgpt from '../assets/next/chat-gpt.webp'
 import { NextJs, Tailwind, TypeScript, Zustand } from '../icons/skills'
 
 export const nextjs = [{
@@ -19,11 +20,11 @@ export const nextjs = [{
   url: 'https://rick-morty-game-eight.vercel.app',
   github: 'https://github.com/cafesit8/Rick-Morty-game'
 }, {
-  title: 'Guest the Character',
-  img: guest,
+  title: 'ChatGPT Clone',
+  img: chatgpt,
   description: 'Inkfinity es un sitio web donde puedes explorar y añadir libros a tu lista de lectura. Utilizando React/Next.js y Tailwind, desarrollé esta prueba técnica para el evento de midudev. Con filtros por categoría, número de páginas y una función de búsqueda, encontrar tu libro favorito es fácil y rápido. La interfaz es intuitiva y responsive, brindando una experiencia de usuario fluida en cualquier dispositivo. Demuestro mis habilidades en desarrollo frontend al utilizar componentes en React y estilos con Tailwind CSS.',
   skills: [NextJs, TypeScript, Zustand, Tailwind],
-  bg: 'bg-[#b94e21]',
-  url: 'https://rick-morty-game-eight.vercel.app',
-  github: 'https://github.com/cafesit8/Rick-Morty-game'
+  bg: 'bg-[#449b84]',
+  url: 'chatgpt-clone-cafesit8.vercel.app',
+  github: 'https://github.com/cafesit8/chat-gpt-clone'
 }]
